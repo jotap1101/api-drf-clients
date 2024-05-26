738aaddebb7325de2604b5d8fb9bc9f48720ad2d
jotap1101 <joaopedroalves.notebook1101@gmail.com>
2024-05-26 10:24:23 -0300

    feat: ajustar validators

--- client/validators.py
+++ client/validators.py
@@ -8,8 +8,7 @@ def rg_validator(value):
     return re.match(r'^\d{2}\.\d{3}\.\d{3}-\d$|^\d{2}\.\d{3}\.\d{3}$|^\d{8}$|^\d{9}$', value) # 00.000.000-0, 00.000.000, 00000000, 000000000
 
 def cpf_validator(value):
-    # return re.match(r'^\d{3}\.\d{3}\.\d{3}-\d{2}$|^\d{11}$', value) # 000.000.000-00, 00000000000
-    cpf = CPF()
+    cpf = CPF() # return re.match(r'^\d{3}\.\d{3}\.\d{3}-\d{2}$|^\d{11}$', value) # 000.000.000-00, 00000000000
 
     return cpf.validate(value) # CPF válido (utilizando a biblioteca validate_docbr)
 
